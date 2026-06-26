f2e71a60a33fdc6553d90d3d821148320826d825
qooke <83923036@qq.com>
2026-06-26 17:32:15 +0900

    mediatek: add support for MelonPi M81 Pro
    Specifications:
    
    - SoC: MediaTek MT7981B
    - RAM: 512 MiB DDR3
    - Flash: 128 MiB SPI-NAND
    - WiFi: MediaTek MT7976C
    - LAN: 1x 1GbE
    - WAN: 1x 2.5GbE (Airoha EN8811H)
    - USB: 1x USB 3.0
    - M.2: 1x M.2 Key-B slot for 5G modem
    - SIM: 1x Nano-SIM slot
    - Fan: 1x 4-pin PWM fan header
    - Buttons: Reset, WPS
    - LEDs: PWR, SYS, NET
    - Power: USB Type-C PD, 12V/2A
    
    Installation:
    
    1. Open the enclosure.
    2. Solder a 3-Pins GND, RX, TX, UART header on J4.
    3. Connect to the serial console (115200 8N1).
    4. Interrupt U-Boot.
    5. Flash the factory image using:
    
       mtkupgrade <board>-sysupgrade.itb
    
    6. Reboot.
    
    Signed-off-by: qooke <83923036@qq.com>

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -2845,6 +2845,31 @@ define Device/openfi_6c
 endef
 TARGET_DEVICES += openfi_6c
 
+define Device/melonpi_m81-pro
+  DEVICE_VENDOR := MelonPi
+  DEVICE_MODEL := M81 Pro
+  DEVICE_DTS := mt7981b-melonpi-m81-pro
+  DEVICE_DTS_DIR := ../dts
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  KERNEL_IN_UBI := 1
+  UBOOTENV_IN_UBI := 1
+  IMAGES := sysupgrade.itb
+  KERNEL_INITRAMFS_SUFFIX := -recovery.itb
+  KERNEL := kernel-bin | gzip
+  KERNEL_INITRAMFS := kernel-bin | lzma | \
+	fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd | pad-to 64k
+  IMAGE/sysupgrade.itb := append-kernel | \
+	fit gzip $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb external-static-with-rootfs | append-metadata
+  DEVICE_PACKAGES := kmod-mt7915e kmod-mt7981-firmware mt7981-wo-firmware kmod-phy-airoha-en8811h \
+  kmod-hwmon-pwmfan kmod-usb3 kmod-usb-serial-option kmod-usb-net-cdc-ether kmod-usb-net-qmi-wwan 
+  ARTIFACTS := preloader.bin bl31-uboot.fip
+  ARTIFACT/preloader.bin := mt7981-bl2 spim-nand-ddr3
+  ARTIFACT/bl31-uboot.fip := mt7981-bl31-uboot melonpi_m81-pro
+endef
+TARGET_DEVICES += melonpi_m81-pro
+
 define Device/openwrt_one
   DEVICE_VENDOR := OpenWrt
   DEVICE_MODEL := One
